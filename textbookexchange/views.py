532d688c06a7f0305623692e9f562b586af06ec2
Mark Sherriff <sherriff@virginia.edu>
2020-09-01 14:24:29 -0400

    2:00 is awesome

--- textbookexchange/views.py
+++ textbookexchange/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hi 11:00 class!!! Hello, world. You're at the textbook exchange index.  Cool test!  Yes it is!")
+    return HttpResponse("Hi 2:00 class!!! Hello, world. You are my everything!  :heart:")
