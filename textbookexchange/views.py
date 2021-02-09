3ae718c4798709a5e8b14f54888e5c02a8a778dd
Mark Sherriff <sherriff@virginia.edu>
2021-02-09 14:45:25 -0500

    ag adsdkljasdfjklasdf

--- textbookexchange/views.py
+++ textbookexchange/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("How DARE those ner'do'wells in 11:00 haxor my system!  <angry face/>  2:00 is awesome and I love you all :heart:")
+    return HttpResponse("How DARE those ner'do'wells in 11:00 haxor my system!  <angry face/>  2:00 is awesome and I love you all :heart: asdgsdfvsdsgfdagassdgfag")
