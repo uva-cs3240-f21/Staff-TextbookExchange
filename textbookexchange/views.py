59b10a6af50492a73a3d22a6e9518054bb27801d
Mark Sherriff <sherriff@virginia.edu>
2021-02-09 11:37:14 -0500

    example for 11:00

--- textbookexchange/views.py
+++ textbookexchange/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hi all class!!! Hello, world. You are my everything!  :heart:")
+    return HttpResponse("Hi 11:00!!! You are my besties.  Don't tell 2:00.  I'll delete this from the recording.  Hello, world. You are my everything!  :heart:")
